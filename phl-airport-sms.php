c76e4594b45ad7d88d1948e1f4c08721299f76cc
Alex Castro <acastro917@gmail.com>
2012-01-17 15:13:51 -0500

    Update phl-airport-sms.php

--- phl-airport-sms.php
+++ phl-airport-sms.php
@@ -8,7 +8,7 @@
 define("SCRAPERWIKI_API_URL", "http://api.scraperwiki.com/api/1.0/datastore/sqlite");
 define("SCRAPERWIKI_FORMAT", "jsondict");
 define("SCRAPERWIKI_NAME", "table2");
-define("SCRAPERWIKI_APYKEY", "609390aa-1d9c-4961-8b8e-b5875f36f426");
+define("SCRAPERWIKI_APIKEY", "609390aa-1d9c-4961-8b8e-b5875f36f426");
 define("SCRAPERWIKI_QUERY", "select%20*%20from%20%60swdata%60%20where%20%60flight_num%60%20%3D%20%22[[flight_num]]%22%20and%20date%20%3D%20%22[[date]]%22%20and%20%60flight_type%60%20%3D%20%22[[direction]]%22");
 
 // Function to fetch JSON for a specific flight from Scrapewiki.
